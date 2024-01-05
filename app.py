9e8421e26fbd2c0065cf6cb8266e5a5698ce3b07
cees <ceeskamu@gmail.com>
2024-01-05 17:35:21 +0700

    c

--- app.py
+++ app.py
@@ -7,7 +7,68 @@ import threading
 start_time = time.time()
 antre =[]
 app = Flask(__name__)
-
+database={}
 @app.route('/')
 def hello_world():
     return 'Hello, world!<br>Myname Muhamad Idris'
+def check(proxy,tok,hasil,pool):
+  if len(str(hasil))>5:return
+  proxy = {
+    'http': 'socks5://'+proxy,
+    'https': 'socks5://'+proxy
+}
+  try:
+    ses=requests.Session()
+    host= 'https://d0o0d.com'
+    ses.proxies.update(proxy)
+    log2=ses.get(host+"/e/"+tok,headers={'Host': 'd0o0d.com', 'referer': 'https://d0o0d.com/e/', 'accept-encoding': 'gzip', 'user-agent': 'okhttp/4.9.0'},timeout=3)
+    if not 'ddos' in log2.text.lower():
+        link=host+"/pass_md5/"+re.search("/pass_md5/(.*?)', function",str(log2.text)).group(1)
+        result = ses.get(link,headers={"Host": host.replace('https://',''),"referer": log2.url,"accept-encoding": "gzip","cookie": "lang=1","user-agent": "okhttp/4.9.0"},timeout=3).text+"".join([random.choice('abcdefghijklmnopqrstuvwxyz1234567890') for _ in range(10)])+"?token="+link.split("/")[-1]+"&expiry=1"+"".join([str(random.randrange(1,9)) for _ in range(12)])
+        ini = ses.get(result,headers={'Range': 'bytes=0-', 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) coc_coc_browser/83.0.144 Chrome/77.0.3865.144 Safari/537.36', 'Referer': 'https://dooood.com/', 'Connection': 'Keep-Alive', 'Accept-Encoding': 'gzip'},stream=True,timeout=3)
+        hasil.update({'response':ini,'headers':ini.headers})
+        pool.shutdown()
+    else:pass
+  except Exception as e :pass
+@app.route('/d/')
+def unduh():
+  try:
+      tok = request.args.get('token')
+      if tok:
+          pass
+      else:
+          return {'return': 'need params token'}
+
+      proxy = requests.get(
+          'https://api.proxyscrape.com/?request=displayproxies&proxytype=socks5&timeout=10000&country=all&ssl=all&anonymity=all').text
+      proxies = np.char.replace(proxy.split('\n')[:-1], '\r', '')
+      hasil={}
+      file='dood/'+tok+'.mp4'
+      with ThreadPoolExecutor(max_workers=20) as pool:
+          for proxy in proxies:
+              pool.submit(check, proxy, tok,hasil,pool)
+      runtime = round(time.time() - start_time,2)
+      
+      try:
+          heads = hasil['headers']
+          threading.Thread(target=build,args=(hasil['response'],tok)).start()
+          return {'runtimeAPI':runtime,'result':'generating','size':str(hasil['headers']['Content-Length']),'warning':'wait for generate content'}
+      except Exception as e:
+          if tok in database:
+              return {'runtimeAPI':runtime,'result':'succes','size':str(len(database[tok]))}
+      
+      
+          
+
+  except Exception as e:
+      return {'result': str(e)}
+
+def build(ini,tok):
+    global database
+    achunk=b''
+    try:
+        
+        for chunk in ini.raw.stream(10485, decode_content=False):
+            achunk+=chunk
+    except Exception as e:pass
+    database.update({tok:achunk})
