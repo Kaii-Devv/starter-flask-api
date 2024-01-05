1d9b96799f47f53ffc156bf75e90b8ff2c9c9f6f
cees <ceeskamu@gmail.com>
2024-01-05 17:05:54 +0700

    2v

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask
 import os
-
+from flask import Flask, send_file, request, jsonify, render_template, Response
+import requests, re, io,os,asyncio,subprocess,speedtest
+import random,time
+start_time = time.time()
+antre =[]
+import threading
+import numpy as np
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, world!'
+    return 'Hello, world!<br>Myname Muhamad Idris'
