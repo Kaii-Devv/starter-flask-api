546da9543a7dfdf7f595721e9f542ec38291188c
Your Name <you@example.com>
2024-03-19 18:57:00 +0700

    o

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def all():
     x = []
     for obj in response["Contents"]:
         try:
-            x.append(obj.get('key'))
+            x.append(obj
         except:pass
     return {'resut':x}
 
