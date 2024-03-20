a1946ab9a3115479c60ef4d4afd2b937bb5ca0d9
Your Name <you@example.com>
2024-03-20 14:08:07 +0700

    p

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ def imagegen():
         size='1:1'
     if prompt:
         try:
-            result = generateImagev2(prompt.replace("+"," "),style=styleId)
+            result = generateImagev2(prompt.replace("+"," "),style=styleId,size=size)
             upData(requests.get(result,stream=True).content,key=result.split('/')[-1])
             return {'author':'Muhamad Idris','result':'succes','patch':'/content/'+result.split('/')[-1]}
         except Exception as e:return {'error':str(e)}
